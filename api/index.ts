4fce6bb4ab61846762f496ca4b74f6f2d0d576ad
ikpeku <ikpedaniel07@gmail.com>
2025-06-20 21:10:03 +0100

    vercel deployment test

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import app from '../src/app';
+import app from '../src/server';
+// import app from '../src/app';
 // import app from '../src/index';
 
 export default (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import logger from "./middlewares/logger.middleware";
 
-export const PORT = process.env.PORT || 9871;
+const PORT = process.env.PORT || 9871;
 
 (async () => {
   logger.info(`Attempting to run server on port ${PORT}`);
@@ -12,7 +12,7 @@ export const PORT = process.env.PORT || 9871;
   
 })();
 
-
+export default app;
 
 
 
