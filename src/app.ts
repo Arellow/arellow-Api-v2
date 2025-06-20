37639824704b90fd1c5ee1e8b3f8d6dc45503219
ikpeku <ikpedaniel07@gmail.com>
2025-06-20 20:05:53 +0100

    modify route

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import express from "express";
 import indexMiddleware from "./middlewares/index.middleware";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./docs/swagger";
+import errorHandler from './middlewares/errors.middleware';
 
 const app = express();
 indexMiddleware(app);
 
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+  // Custom error handling middleware - moved after routes
+  app.use(errorHandler);
 
 export default app;
