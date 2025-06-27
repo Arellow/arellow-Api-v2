73d653757f412c563c849d6377f186042bd6b66c
ikpeku <ikpedaniel07@gmail.com>
2025-06-27 23:48:11 +0100

    clear log

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,3 @@
-// import { PrismaClient } from '@prisma/client'
-
-// export const Prisma = new PrismaClient()
-
-
-// import { PrismaClient } from '@prisma/client';
-
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const Prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = Prisma;
-
-
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
