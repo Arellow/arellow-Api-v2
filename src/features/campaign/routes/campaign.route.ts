1e67bb9b90af42db1b07355af4407b39327e78e9
ikpeku <ikpedaniel07@gmail.com>
2025-10-18 21:30:48 +0100

    add campaign

--- src/features/campaign/routes/campaign.route.ts
+++ src/features/campaign/routes/campaign.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { singleupload } from "../../../middlewares/multer";
 import { AllActiveCampaigns, AllCampaigns, clickCampaign, createCampaign, deleteCampaign, updateCampaign } from "../contollers/controller";
 import authenticate, { adminRequireRole, isSuspended, requireRole } from "../../../middlewares/auth.middleware";
-import { UserRole } from "@prisma/client";
+import { CampaignAddress, CampaignPlaceMent, UserRole } from "@prisma/client";
 import { createCampaignSchema } from "./campaign.validate";
 import { validateSchema } from "../../../middlewares/propertyParsingAndValidation";
 
@@ -11,8 +11,39 @@ const campaignRoutes = Router();
 
 campaignRoutes.get("/active", AllActiveCampaigns);
 campaignRoutes.get("/", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"), AllCampaigns);
-campaignRoutes.post("/", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"),  singleupload, validateSchema(createCampaignSchema), createCampaign);
-campaignRoutes.patch("/update/:id", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"),  singleupload,  validateSchema(createCampaignSchema), updateCampaign);
+campaignRoutes.post("/", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"),  singleupload, (req, res, next) => {
+
+      const parsedCampaignPlaceMent: CampaignPlaceMent[] = typeof req.body.campaignPlaceMent === 'string' ? JSON.parse(req.body.campaignPlaceMent) : req.body.campaignPlaceMent;
+    
+      const parsedCampaignAddress: CampaignAddress = typeof req.body.campaignAddress === 'string' ? JSON.parse(req.body.campaignAddress) : req.body.campaignAddress;
+
+        const body = {
+        ...req.body,
+       campaignAddress: parsedCampaignAddress,
+        campaignPlaceMent: parsedCampaignPlaceMent,
+    };
+    req.body = body;
+    next()
+},
+
+validateSchema(createCampaignSchema), createCampaign);
+
+campaignRoutes.patch("/update/:id", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"),  singleupload,(req, res, next) => {
+
+      const parsedCampaignPlaceMent: CampaignPlaceMent[] = typeof req.body.campaignPlaceMent === 'string' ? JSON.parse(req.body.campaignPlaceMent) : req.body.campaignPlaceMent;
+    
+      const parsedCampaignAddress: CampaignAddress = typeof req.body.campaignAddress === 'string' ? JSON.parse(req.body.campaignAddress) : req.body.campaignAddress;
+
+        const body = {
+        ...req.body,
+       campaignAddress: parsedCampaignAddress,
+        campaignPlaceMent: parsedCampaignPlaceMent,
+    };
+    req.body = body;
+    next()
+},
+
+validateSchema(createCampaignSchema), updateCampaign);
 campaignRoutes.patch("/:id", clickCampaign);
 campaignRoutes.delete("/:id", authenticate, isSuspended, requireRole(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminRequireRole("CAMPAIGN"),  deleteCampaign);
 
