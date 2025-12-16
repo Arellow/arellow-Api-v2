4b32cd0c5851886d5dd04dd72d90594316cbae76
ikpeku <ikpedaniel07@gmail.com>
2025-12-16 04:04:59 +0100

    remove mortage calculator

--- src/features/mortgage/routes/mortagage.ts
+++ src/features/mortgage/routes/mortagage.ts
@@ -1,7 +1,5 @@
 import express from 'express'
-import authenticate from '../../../middlewares/auth.middleware';
-import { calculateCustomMortgage } from '../controllers/calculate';
+
 const mortagageRoutes = express.Router();
 
-mortagageRoutes.post("/calculate",authenticate, calculateCustomMortgage)
 export default mortagageRoutes
