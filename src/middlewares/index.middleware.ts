03b2f53f5c1327cf3949a5c3be7d34782664c4e6
ikpeku <ikpedaniel07@gmail.com>
2026-02-28 02:17:31 +0100

    handle cors

--- src/middlewares/index.middleware.ts
+++ src/middlewares/index.middleware.ts
@@ -2,7 +2,6 @@
 import express, { Application, json, urlencoded } from "express";
 import { configDotenv } from "dotenv";
 import morgan from "morgan";
-import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import indexRoutes from "../features/appRoute";
@@ -12,12 +11,7 @@ export default (app: Application) => {
   // Logging middleware
   app.use(morgan("combined"));
 
-  // CORS middleware 
-  app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+
 
   // Configuration setup (dotenv)
   if (process.env.NODE_ENV !== 'production') configDotenv();
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,8 @@
-// import logger from "./middlewares/logger.middleware";
-// import { PORT } from "./utils/constants.util";
-// import 'dotenv/config';
-// import express from "express";
-// import indexMiddleware from "./middlewares/index.middleware";
-// import errorHandler from './middlewares/errors.middleware';
-// import { isLoginUser } from "./middlewares/auth.middleware";
-// import { setupSocket } from "./socketServer";
-// import http from "http";
-
-// const app = express();
-
-// const httpServer = http.createServer(app);
-// const io = setupSocket(httpServer);
-// app.use(isLoginUser)
-// indexMiddleware(app)
-
-// app.use("/", async(req, res) => {
-
-
-//     res.send("welcome to arellow home");
-
-// });
-
-// // Custom error handling middleware - moved after routes
-// app.use(errorHandler);
-
-// (async () => {
-//   logger.info(`Attempting to run server on port ${PORT}`);
- 
-//   httpServer.listen(PORT, () => {
-//     logger.info(`Listening on port ${PORT}`);
-//   });
-  
-// })();
-
-// export default app;
-
-
 import "dotenv/config";
 import "express-async-errors";
 import express from "express";
 import http from "http";
-
+import cors from "cors";
 import logger from "./middlewares/logger.middleware";
 import { PORT } from "./utils/constants.util";
 
@@ -54,6 +15,13 @@ import { initSocketIO } from "./features/userchat/route/socketServer";
 const app = express();
 
 const server = http.createServer(app);
+
+  // CORS middleware 
+  app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 // assign user to API routes
 app.use(isLoginUser)
